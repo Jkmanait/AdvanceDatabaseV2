d665e1ddb9ff9b2585cf86380fd8a9c03e78476a
jkmanait <jkmanait@gmail.com>
2023-12-11 22:42:10 +0800

    finalssssss

--- cart.php
+++ cart.php
@@ -8,7 +8,7 @@ include('functions/common_function.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Commerce Website</title>
+    <title>E-Commerce Website-Cart Details</title>
     <!-- boostrap CSS link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
     rel="stylesheet" 
@@ -18,6 +18,13 @@ include('functions/common_function.php');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />     
      <!--css file  -->
      <link rel="stylesheet" href="style.css">
+     <style>
+        .cart_img{
+    width: 80px;
+    height: 80px;
+    object-fit: contain;
+}
+     </style>
 </head>
 <body>
     <!-- navbar -->
@@ -48,15 +55,7 @@ include('functions/common_function.php');
           cart_item();?>
           </sup></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Total Price: ₱<?php total_cart_price();?></a>
-        </li>
       </ul>
-      <form class="d-flex" action="search_product.php" method="get">
-        <input class="form-control me-2" type="search" 
-        placeholder="Search" aria-label="Search" Name="search_data">
-        <input type="submit" value="Search" class="btn btn-outline-light" name="search_data_product">
-      </form>
     </div>
   </div>
 </nav>
@@ -83,46 +82,67 @@ cart();
     <p class="text-center">"Clothes aren’t going to change the world, but the person wearing them might."</p>
 </div>
 
-<!-- fourth child -->
-<div class="row px-1">
-  <div class="col-md-10">
-    <!-- products -->
+<!-- fourth child-table -->
+<div class="container">
     <div class="row">
-<!-- fetching products -->
-    <?php
-    // calling function
-getproducts();
-get_unique_categories();
-get_unique_brands();
-// $ip = getIPAddress();  
-// echo 'User Real IP Address - '.$ip;
-    ?>
-<!-- row end -->
-</div>
-<!-- col end -->
-</div>
-  <div class="col-md-2 bg-secondary p-0">
-    <!-- brands to be displayed -->
-    <ul class="navbar-nav me-auto text-center">
-      <li class="nav-item bg-info">
-        <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
-      </li>
-      <?php
-getbrands();
-      ?>
-    </ul>
+        <table class="table table-bordered text-center">
+            <thead>
+                <tr>
+                    <th>Product Title</th>
+                    <th>Product Image</th>
+                    <th>Quantity</th>
+                    <th>Total Price</th>
+                    <th>Remove</th>
+                    <th colspan="2">Operations</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- php code to display dynamic data -->
+                <?php
+    global $con;
+    $get_ip_add = getIPAddress();
+    $total_price=0;
+    $cart_query="Select * from `cart_details` where ip_address='$get_ip_add'";
+    $result=mysqli_query($con,$cart_query);
+    while($row=mysqli_fetch_array($result)){
+        $product_id=$row['product_id'];
+        $select_products="Select * from `Products` where product_id='$product_id'";
+        $result_products=mysqli_query($con,$select_products);
+        while($row_product_price=mysqli_fetch_array($result_products)){
+            $product_price=array($row_product_price['product_price']);
+            $price_table=$row_product_price['product_price'];
+            $product_title=$row_product_price['product_title'];
+            $product_image1=$row_product_price['product_image1'];
+            $product_values=array_sum($product_price);
+            $total_price+=$product_values;
+    
+    
+                ?>
+                <tr>
+                    <td><?php echo $product_title ?></td>
+                    <td><img src="./images/<?php echo $product_image1 ?>" alt="" class="cart_img"></td>
+                    <td><input type="text" name="" id="" class="form-input w-50"></td>
+                    <td>₱<?php echo $price_table ?></td>
+                    <td><input type="checkbox"></td>
+                    <td>
+                        <button class="bg-info px-3 py-2 mx-2">Update</button><button class="bg-info px-3 py-2 mx-2">Remove</button>
+                    </td>
+                </tr>
 
-    <!-- categories to be displayed -->
-    <ul class="navbar-nav me-auto text-center">
-      <li class="nav-item bg-info">
-        <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
-      </li>
-      <?php
-getcategories();
-      ?>
-    </ul>
-  </div>
+                <?php }
+    }?>
+            </tbody>
+        </table>
+        <!-- SubTotal -->
+        <div class="d-flex mb-4">
+            <h4 class="px-3">Subtotal:<strong class="text-info"> ₱<?php echo $total_price ?></strong></h4>
+            <a href="index.php"><button class="bg-info px-3 py-2 border-0 mx-3">Continue Shopping</button></a>
+            <a href="#"><button class="bg-secondary p-3 py-2 border-0 text-light">Checkout</button></a>
+        </div>
+    </div>
 </div>
+
+
 
 <!-- last child -->
 <div class="bg-info p10 text-center">
--- functions/common_function.php
+++ functions/common_function.php
@@ -28,6 +28,7 @@ while($row=mysqli_fetch_assoc($result_query)){
             <div class='card-body'>
             <h5 class='card-title'>$product_title</h5>
             <p class='card-text'>$product_description</p>
+            <p class='card-text'>Price: ₱$product_price</p>
             <a href='index.php?Add_to_cart=$product_id' class='btn btn-primary'>Add To Cart</a>
             <a href='#' class='btn btn-secondary'>Buy Now</a>
             </div>
@@ -69,6 +70,7 @@ while($row=mysqli_fetch_assoc($result_query)){
             <div class='card-body'>
             <h5 class='card-title'>$product_title</h5>
             <p class='card-text'>$product_description</p>
+            <p class='card-text'>Price: ₱$product_price</p>
             <a href='index.php?Add_to_cart=$product_id' class='btn btn-primary'>Add To Cart</a>
             <a href='#' class='btn btn-secondary'>View More</a>
             </div>
@@ -110,6 +112,7 @@ while($row=mysqli_fetch_assoc($result_query)){
             <div class='card-body'>
             <h5 class='card-title'>$product_title</h5>
             <p class='card-text'>$product_description</p>
+            <p class='card-text'>Price: ₱$product_price</p>
             <a href='index.php?Add_to_cart=$product_id' class='btn btn-primary'>Add To Cart</a>
             <a href='#' class='btn btn-secondary'>View More</a>
             </div>
@@ -173,6 +176,7 @@ if($num_of_rows==0){
                 <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
+                <p class='card-text'>Price: ₱$product_price</p>
                 <a href='index.php?Add_to_cart=$product_id' class='btn btn-primary'>Add To Cart</a>
                 <a href='#' class='btn btn-secondary'>View More</a>
                 </div>
@@ -183,11 +187,83 @@ if($num_of_rows==0){
 }
 
 
+// get ip address function
+function getIPAddress() {  
+    //whether ip is from the share internet  
+     if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+        }  
+    //whether ip is from the proxy  
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+     }  
+//whether ip is from the remote address  
+    else{  
+             $ip = $_SERVER['REMOTE_ADDR'];  
+     }  
+     return $ip;  
+}  
+// $ip = getIPAddress();  
+// echo 'User Real IP Address - '.$ip;  
+
+
 // cart function
 function cart(){
-if(isset($_GET['Add_to_cart'])){
+    if(isset($_GET['Add_to_cart'])){
+        global $con;
+        $get_ip_add = getIPAddress();
+        $get_product_id=$_GET['Add_to_cart'];
+        $select_query="Select * from `cart_details` where ip_address='$get_ip_add' and product_id=$get_product_id";
+        $result_query=mysqli_query($con,$select_query);
+        $num_of_rows=mysqli_num_rows($result_query);
+if($num_of_rows>0){
+    echo "<script>alert('This item is already present inside the cart')</script>";
+    echo "<script>window.open('index.php','_self')</script>";
+}else{
+    $insert_query="insert into `cart_details` (product_id,ip_address,quantity) values ($get_product_id,'$get_ip_add',0)";
+    $result_query=mysqli_query($con,$insert_query);
+    echo "<script>alert('Item is added to cart')</script>";
+    echo "<script>window.open('index.php','_self')</script>";
+}
+}
+}
+
+// function to get cart item numbers
+function cart_item(){
+    if(isset($_GET['Add_to_cart'])){
+        global $con;
+        $get_ip_add = getIPAddress();
+        $select_query="Select * from `cart_details` where ip_address='$get_ip_add'";
+        $result_query=mysqli_query($con,$select_query);
+        $count_cart_items=mysqli_num_rows($result_query);
+}else{
     global $con;
-    
+        $get_ip_add = getIPAddress();
+        $select_query="Select * from `cart_details` where ip_address='$get_ip_add'";
+        $result_query=mysqli_query($con,$select_query);
+        $count_cart_items=mysqli_num_rows($result_query);
 }
+echo $count_cart_items;
 }
+
+// total price function
+function total_cart_price(){
+    global $con;
+    $get_ip_add = getIPAddress();
+    $total_price=0;
+    $cart_query="Select * from `cart_details` where ip_address='$get_ip_add'";
+    $result=mysqli_query($con,$cart_query);
+    while($row=mysqli_fetch_array($result)){
+        $product_id=$row['product_id'];
+        $select_products="Select * from `Products` where product_id='$product_id'";
+        $result_products=mysqli_query($con,$select_products);
+        while($row_product_price=mysqli_fetch_array($result_products)){
+            $product_price=array($row_product_price['product_price']);
+            $product_values=array_sum($product_price);
+            $total_price+=$product_values;
+        }
+    }
+    echo $total_price;
+}
+
 ?>
